f96810132f5fc984534aa2e89d1184dfe4832368
dx-pdemo <pdemo2020@163.com>
2021-01-06 23:53:04 +0800

    第一个java

---
+++ src/main/java/第一个java/FirstJavaDemo.java
@@ -0,0 +1,16 @@
+package 第一个java;
+
+/**
+ * @Author: Dx-lipde
+ * @Date: 2021/1/6 23:50
+ * @Version: v1
+ * @Description:
+ */
+// define a class ， public class className 主类
+public class FirstJavaDemo {
+    // main method  入口 所有程序的开始
+    public static void main(String[] args) {
+        // 打印输出到控制台  println 换行 print 不换行
+        System.out.println("第一个java");
+    }
+}
